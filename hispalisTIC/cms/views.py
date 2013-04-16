f4d700384db616a22cad01fd2e7b28541cb39b38
Rafael Romero <rafaelromcar@gmail.com>
2013-04-16 21:51:18 +0200

    Deleted menu views and added login decorators

--- hispalisTIC/cms/views.py
+++ hispalisTIC/cms/views.py
@@ -1,7 +1,11 @@
-from cms.models import Page, Post, Category, CategoryForm, PostForm, PageForm
+from cms.models import Page, Post, Category, Technology, Service
+from cms.forms import PostForm, CategoryForm, PageForm, TechnologyForm, ServiceForm
 
-from django.shortcuts import render_to_response, get_object_or_404, render
+from django.shortcuts import render_to_response, get_object_or_404, render, redirect
 from django.http import HttpResponseRedirect
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
 
 # General views
 
@@ -27,9 +31,21 @@ def view_page(request, slug):
 	return render_to_response('view_page.html', {
 		'page': get_object_or_404(Page, slug=slug)
 	})
+'''
+def view_category(request, slug):
+	return render_to_response('view_page.html', {
+		'page': get_object_or_404(Category, slug=slug)
+	})
+'''
+def view_service(request, slug):
+	return render_to_response('view_page.html', {
+		'page': get_object_or_404(Page, slug=slug)
+	})
+
 
 # Forms views
-	
+
+@login_required
 def new_category_form(request):
 	if request.method == 'POST':
 		form = CategoryForm(request.POST)
@@ -40,6 +56,7 @@ def new_category_form(request):
 		form = CategoryForm()
 	return render(request, "new_category.html", {'form' : form})
 
+@login_required
 def new_post_form(request):
 	if request.method == 'POST':
 		form = PostForm(request.POST)
@@ -50,6 +67,7 @@ def new_post_form(request):
 		form = PostForm()
 	return render(request, "new_post.html", {'form' : form})
 
+@login_required
 def new_page_form(request):
 	if request.method == 'POST':
 		form = PageForm(request.POST)
@@ -60,12 +78,4 @@ def new_page_form(request):
 		form = PageForm()
 	return render(request, "new_page.html", {'form' : form})
 
-def new_menu_form(request):
-	if request.method == 'POST':
-		form = MenuForm(request.POST)
-		if form.is_valid():
-			form.save()
-			return HttpReponseRedirect('')
-	else:
-		form = MenuForm()
-	return render(request, "new_menu.html", {'form' : form})
+
