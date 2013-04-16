0c6d2295d6ef538ad85bae0e830b7740eae9ec1a
Rafael Romero <rafaelromcar@gmail.com>
2013-04-16 21:52:03 +0200

    Added admins to Page, Post, Category, Technology and Service

--- hispalisTIC/cms/admin.py
+++ hispalisTIC/cms/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-from models import Post, Category
-
-class PostAdmin(admin.ModelAdmin):
-	exclude = ['posted']
-	prepopulated_fields = {'slug': ('title',)}
-
-class CategoryAdmin(admin.ModelAdmin):
-	prepopulated_fields = {'slug': ('title',)}
+from models import Post, Category, Page, Technology, Service
 
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(Page)
+admin.site.register(Technology)
+admin.site.register(Service)
